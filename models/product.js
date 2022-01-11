74939b8352498526458c2e4e4250cd7a3665d8c8
Avinash Kumar <44208272+avinashmallik62@users.noreply.github.com>
2022-01-11 21:45:15 +0530

    Add files via upload

--- models/product.js
+++ models/product.js
@@ -1,19 +1,27 @@
 const mongoose = require("mongoose");
-
+const Review = require("./review");
 const productSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
   },
   img: {
     type: String,
   },
   price: {
     type: Number,
+    min: 0,
   },
   desc: {
     type: String,
   },
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
-const Product = mongoose.model("Product", productSchema);
 
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { use } = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const Product = require("./product");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  cart: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
